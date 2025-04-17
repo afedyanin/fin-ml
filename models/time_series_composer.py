127f1afcda871504f420d0c13b0869741c759699
DESKTOP-MAIBTV1\A <anfemail@gmail.com>
2025-04-17 18:37:03 +0300

    update

--- models/time_series_composer.py
+++ models/time_series_composer.py
@@ -1,3 +1,17 @@
+import pandas as pd
+
+
 class TimeSeriesComposer:
     def __init__(self):
         pass
+
+    @staticmethod
+    def shift_column(df: pd.DataFrame,
+                     col_name: str,
+                     count: int = 1,
+                     shifted_col_suffix: str = '_T-') -> pd.DataFrame:
+        for i in range(count):
+            shifted_name = col_name + shifted_col_suffix + str(i+1)
+            df[shifted_name] = df[col_name].shift(i+1)
+        return df
+
